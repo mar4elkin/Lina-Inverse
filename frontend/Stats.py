6cf9b47906e67d3a293480ffae14ae084fea1690
Mark Tamarov <googlcraftlol@gmail.com>
2021-02-04 02:15:32 +0300

    fixing stats

--- frontend/Stats.py
+++ frontend/Stats.py
@@ -20,22 +20,36 @@ class Stats(object):
                 ovData = OverwatchWorker.getUserInfo(row.battleId)
                 ovOldData = ast.literal_eval(row.overwatch_data)
                 blogger(f"Checking rows in Stats.py ovData: {ovData} ovOldData {ovOldData} for user {row.battleId}")
-                if (ovData['games']['played'] > ovOldData['games']['played']):
-                    blogger(f"games new {ovData['games']['played']} and old {ovOldData['games']['played']}")
-                    blogger(f"ranks dd new {ovData['ranks']['damage']['rank']} and old {ovOldData['ranks']['damage']['rank']}")
-                    blogger(f"ranks support new {ovData['ranks']['support']['rank']} and old {ovOldData['ranks']['support']['rank']}")
-                    blogger(f"ranks tank new {ovData['ranks']['tank']['rank']} and old {ovOldData['ranks']['tank']['rank']}")
-                    OverwatchWorker.updateUser(
-                        str(row.battleId), 
-                        str(ovData), 
-                        str(ovOldData)
-                    )
-                    igen = ImageGen('overwatch', ovData, ovOldData)
-                    igen.drawImgOverwatch()
-                    blogger(f"message sended")
-                    await channel.send(f'<@!{row.discordId}>', file=discord.File('tmp/overwatch/overwatch.png'))
-                    #'tmp/overwatch/overwatch.png'
-                    #await channel.send(str(row.overwatch_data))
-                else:
-                    blogger(f"user account didn't reach 25 lvl")
+                if (ovOldData['ranks']['damage'] != {} and ovData['ranks']['damage'] != {}):
+                    if (ovData['ranks']['damage'] != {} and ovOldData['ranks']['damage'] == {}):
+                        blogger(f"games new {ovData['games']['played']} and old {ovOldData['games']['played']}")
+                        blogger(f"ranks dd new {ovData['ranks']['damage']['rank']} and old {ovOldData['ranks']['damage']['rank']}")
+                        blogger(f"ranks support new {ovData['ranks']['support']['rank']} and old {ovOldData['ranks']['support']['rank']}")
+                        blogger(f"ranks tank new {ovData['ranks']['tank']['rank']} and old {ovOldData['ranks']['tank']['rank']}")
+                        OverwatchWorker.updateUser(
+                            str(row.battleId), 
+                            str(ovData), 
+                            str(ovOldData)
+                        )
+                        igen = ImageGen('overwatch', ovData, ovOldData)
+                        igen.drawImgOverwatch()
+                        blogger(f"message sended")
+                        await channel.send(f'<@!{row.discordId}>', file=discord.File('tmp/overwatch/overwatch.png'))
+                    
+                    elif (ovData['games']['played'] > ovOldData['games']['played']):
+                        blogger(f"games new {ovData['games']['played']} and old {ovOldData['games']['played']}")
+                        blogger(f"ranks dd new {ovData['ranks']['damage']['rank']} and old {ovOldData['ranks']['damage']['rank']}")
+                        blogger(f"ranks support new {ovData['ranks']['support']['rank']} and old {ovOldData['ranks']['support']['rank']}")
+                        blogger(f"ranks tank new {ovData['ranks']['tank']['rank']} and old {ovOldData['ranks']['tank']['rank']}")
+                        OverwatchWorker.updateUser(
+                            str(row.battleId), 
+                            str(ovData), 
+                            str(ovOldData)
+                        )
+                        igen = ImageGen('overwatch', ovData, ovOldData)
+                        igen.drawImgOverwatch()
+                        blogger(f"message sended")
+                        await channel.send(f'<@!{row.discordId}>', file=discord.File('tmp/overwatch/overwatch.png'))
+                        #'tmp/overwatch/overwatch.png'
+                        #await channel.send(str(row.overwatch_data))
                 await asyncio.sleep(1200)
